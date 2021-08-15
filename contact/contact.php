50b6e56e83d167248d21e29c8d30f359841fe78a
franborgianifran <86076916+franborgianifran@users.noreply.github.com>
2021-08-15 12:29:02 -0300

    Add files via upload

--- contact/contact.php
+++ contact/contact.php
@@ -15,7 +15,7 @@ $website = $_POST['website'];
 $message = $_POST['message'];
 	
 // Create the email and send the message
-$to = 'yourname@yourdomain.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'franborgiani2003@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nWebsite: $website\n\nMessage:\n$message";
 $headers = "From: noreply@gmail.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
